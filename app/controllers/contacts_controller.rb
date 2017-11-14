9c8f52e851048cb8f44573a19f8a0b5cf02b117c
Arnaud Manaranche <arnaud.mana@gmail.com>
2017-11-14 16:39:56 +0100

    form contact finished => design wip

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,11 @@
 class ContactsController < ApplicationController
-  before_action :set_contact, only: [:show, :edit, :update, :destroy]
+    before_action :set_contact, only: [:show, :edit, :update, :destroy]
 
   # GET /contacts
   # GET /contacts.json
   def index
-    # @contacts = Contact.all
-    @contact = Contact.new
+      # @contacts = Contact.all
+      @contact = Contact.new
   end
 
   # GET /contacts/1
@@ -15,7 +15,7 @@ class ContactsController < ApplicationController
 
   # GET /contacts/new
   def new
-    @contact = Contact.new
+      @contact = Contact.new
   end
 
   # GET /contacts/1/edit
@@ -25,38 +25,34 @@ class ContactsController < ApplicationController
   # POST /contacts
   # POST /contacts.json
   def create
-    @contact = Contact.new(contact_params)
-
-    respond_to do |format|
+      @contact = Contact.new(contact_params)
       if @contact.save
-        format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
-        format.json { render :show, status: :created, location: @contact }
+          redirect_to contacts_path, notice: "Your message has been sent"
       else
-        format.html { render :new }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
+          format.html { render :new }
+          format.json { render json: @contact.errors, status: :unprocessable_entity }
       end
-    end
   end
 
   # PATCH/PUT /contacts/1
   # PATCH/PUT /contacts/1.json
   def update
-    respond_to do |format|
+      respond_to do |format|
       if @contact.update(contact_params)
-        format.html { redirect_to @contact, notice: 'Contact was successfully updated.' }
-        format.json { render :show, status: :ok, location: @contact }
+          format.html { redirect_to @contact, notice: 'Contact was successfully updated.' }
+          format.json { render :show, status: :ok, location: @contact }
       else
-        format.html { render :edit }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
+          format.html { render :edit }
+          format.json { render json: @contact.errors, status: :unprocessable_entity }
       end
     end
-  end
+    end
 
   # DELETE /contacts/1
   # DELETE /contacts/1.json
   def destroy
-    @contact.destroy
-    respond_to do |format|
+      @contact.destroy
+      respond_to do |format|
       format.html { redirect_to contacts_url, notice: 'Contact was successfully destroyed.' }
       format.json { head :no_content }
     end
@@ -65,11 +61,11 @@ class ContactsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_contact
-      @contact = Contact.find(params[:id])
+        @contact = Contact.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
-      params.require(:contact).permit(:name, :email)
+        params.require(:contact).permit(:name, :email, :content)
     end
 end
