243dd8db428dd9068b22eb4e740546eccf36e03b
Victor de la Fouchardière <victor.dlf@outlook.fr>
2017-11-14 18:16:02 +0100

    fix edit comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,7 @@ class CommentsController < ApplicationController
 
     def edit
       @article = Article.find(params[:article_id])  
-      @comment = Comment.find(params[:id])
+      @comment = @article.comments.find(params[:id])
     end
 
     def update
