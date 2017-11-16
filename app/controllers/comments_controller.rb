22486d81b470a6d1447a3d11296ec424673570bf
Arnaud Manaranche <arnaud.mana@gmail.com>
2017-11-16 12:31:16 +0100

    edit comment bug done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,15 +16,15 @@ class CommentsController < ApplicationController
     end
 
     def edit
-      @article = Article.find(params[:article_id])  
+      @article = Article.find(params[:article_id])
       @comment = @article.comments.find(params[:id])
     end
 
     def update
       @comment = Comment.find(params[:id])
-
+      @article = Article.find(params[:article_id])
       if @comment.update(comment_params)
-        redirect_to @comment_article
+        redirect_to article_path(@article)
       else
         render 'edit'
       end
