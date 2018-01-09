070e0ec644744ec282f2136f72d6d4a4a7c35ccb
Arnaud Manaranche <arnaud.mana@gmail.com>
2018-01-09 16:59:42 +0100

    update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
     def index
-        @page_title = "Rubyblognonaise"
+        @page_title = "Rubyblognonaise | Blog"
         @meta_description = "Fortnite Battle Royale est le mode JcJ à 100 joueurs de Fortnite, jouable totalement gratuitement. Une carte géante. Un bus de combat. Tout le système de construction de Fortnite, combiné à des batailles ultraintenses en JcJ. Le dernier survivant est le vainqueur. Disponible sur PC, PlayStation 4, Xbox One et Mac."
         @articles = Article.order('created_at DESC').limit(2)
     end
